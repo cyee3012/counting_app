1ecc485c14dc3ef136520c56bbef0cba78fe2b03
Chii Law <cyee3012@hotmail.co.uk>
2021-11-20 18:24:29 +0900

    deleted package-lock.json. Downgraded bootstrap to version 4.6.0 to get dropdown on nav bar to work

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+ environment.plugins.prepend('Provide',
+   new webpack.ProvidePlugin({
+     $: 'jquery',
+     jQuery: 'jquery',
+     Popper: ['popper.js', 'default']
+   })
+ );
 
 module.exports = environment
